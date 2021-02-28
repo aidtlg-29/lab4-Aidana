cf00da4edd58a18019fb2907194999b3ed5db784
aidtlg-29 <190103164@stu.sdu.edu.kz>
2021-02-28 18:45:28 +0600

    lab4 commit

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\Client;
+use App\Models\Post;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('client/add', function () {
+    DB::table('clients')->insert([
+        'name'=>'Aidana',
+        'age'=>18
+    ]);
+});
+Route::get('client', function () {
+    $client=Client::find(1);
+    return $client;
+});
+
+Route::get('post/create', function () {
+    DB::table('post')->insert([
+        'title'=>'Lab 4 Database',
+        'body'=>'i thought i wouldn"t finish the project,but i did.',
+    ]);
+});
+Route::get('post', function () {
+    $post=Post::find(1);
+    return $post;
+});
+
+
+
+
+
+
