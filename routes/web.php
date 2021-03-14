d7ce596969e2c116290c4274f957d8f407686a7c
aidtlg-29 <190103264@stu.sdu.edu.kz>
2021-03-14 23:58:39 +0600

    laravel

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Post;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('post/create', function () {
+    DB::table('post')->insert([
+        'title'=>'Lab 4 Database',
+        'body'=>'i thought i wouldn\"t finish the project,but i did',
+    ]);
+});
+Route::get('post', function () {
+    $post=Post::find(1);
+    return $post;
+});
+
+
+
+
+
+
